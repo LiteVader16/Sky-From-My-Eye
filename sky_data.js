7f1e764999870bb259ea58139141195c5ebc5ff9
LiteVader16 <mitalivadher@gmail.com>
2026-07-11 22:36:15 +0530

    Add files via upload

---
+++ sky_data.js
@@ -0,0 +1,56 @@
+const skyData = [
+    {
+        "file": "sky_01.jpeg",
+        "location": "Veraval, India",
+        "time": "2026:04:04 19:17:33",
+        "notes": "Click Edit in GitHub on the sky_data.js file to add your notes!"
+    },
+    {
+        "file": "sky_02.jpeg",
+        "location": "Unknown Location",
+        "time": "2025:10:17 04:06:22",
+        "notes": "Click Edit in GitHub on the sky_data.js file to add your notes!"
+    },
+    {
+        "file": "sky_03.jpeg",
+        "location": "Unknown Location",
+        "time": "2025:10:17 04:07:57",
+        "notes": "Click Edit in GitHub on the sky_data.js file to add your notes!"
+    },
+    {
+        "file": "sky_04.jpeg",
+        "location": "Bengaluru, India",
+        "time": "2026:05:12 18:47:36",
+        "notes": "Click Edit in GitHub on the sky_data.js file to add your notes!"
+    },
+    {
+        "file": "sky_05.jpeg",
+        "location": "Haryana, India",
+        "time": "2021:07:24 11:39:05",
+        "notes": "Click Edit in GitHub on the sky_data.js file to add your notes!"
+    },
+    {
+        "file": "sky_06.jpeg",
+        "location": "Unknown Location",
+        "time": "2021:07:24 11:33:33",
+        "notes": "Click Edit in GitHub on the sky_data.js file to add your notes!"
+    },
+    {
+        "file": "sky_07.jpeg",
+        "location": "Perth, Australia",
+        "time": "2025:03:24 09:17:49",
+        "notes": "Click Edit in GitHub on the sky_data.js file to add your notes!"
+    },
+    {
+        "file": "sky_08.jpeg",
+        "location": "Mumbai, India",
+        "time": "2025:05:17 19:12:38",
+        "notes": "Click Edit in GitHub on the sky_data.js file to add your notes!"
+    },
+    {
+        "file": "sky_09.jpeg",
+        "location": "Gujarat, India",
+        "time": "2025:12:12 07:26:00",
+        "notes": "Click Edit in GitHub on the sky_data.js file to add your notes!"
+    }
+];
